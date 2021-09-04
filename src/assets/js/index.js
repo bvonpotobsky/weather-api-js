4a5480c1631f774299b8e03b9406666e9ad80a5a
Benjamin <bvonpotobsky@gmail.com>
2021-09-04 07:29:53 -0400

    refactor makeCard()

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,6 @@
 // Modules
 import { makeCard } from "./utils.js";
 import { handleError } from "./utils.js";
-import { isCityShown } from "./utils.js";
 
 // Catch the DOM
 // const addButton = document.querySelector("#button");
@@ -22,16 +21,24 @@ function getCity(e) {
 async function fetchData(city) {
   const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0829e4d9f43cbab044ad71b0938a0557`;
   // Check if that city has already been shown
-  if (!isCityShown(city)) {
-    try {
-      const response = await fetch(API);
-      const data = await response.json();
-      makeCard(data);
-    } catch (err) {
-      handleError({ err: err });
-    }
-  } else {
-    handleError({ city: city });
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+
+    const city = {
+      id: data.id,
+      name: data.name.toLowerCase(),
+      country: data.sys.country,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      degrees: parseInt(data.main.temp),
+      max: parseInt(data.main.temp_max),
+      min: parseInt(data.main.temp_min),
+    };
+
+    makeCard(city);
+  } catch (err) {
+    handleError({ err: err });
   }
 }
 
--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -6,35 +6,27 @@ const alertMsg = document.querySelector("#alert");
 const cities = [];
 
 // Function to make the Card
-export function makeCard(data) {
-  // Store values
-  console.log(data);
-  const city = data.name;
-  const country = data.sys.country;
-  const description = data.weather[0].description;
-  const icon = data.weather[0].icon;
-  const degrees = parseInt(data.main.temp);
-  const max = parseInt(data.main.temp_max);
-  const min = parseInt(data.main.temp_min);
-  // Make the card
+export function makeCard(city) {
+  // Make the card element
   const card = document.createElement("div");
   card.classList.add("card");
+
   // Make card content
-  const CardHTML = `
-        <h3 class="card__city">${city}, ${country}</h3>
+  const cardHTML = `
+        <h3 class="card__city">${city.name}, ${city.country}</h3>
         <p class="card__update">Last update: 6:52 a.m.</p>
-        <p class="card__degree">${degrees}°</p>
-        <p class="card__description">${description}</p>
+        <p class="card__degree">${city.degrees}°</p>
+        <p class="card__description">${city.description}</p>
         <span class="card__day">
           <img
             class="card__day__icon"
-            src="http://openweathermap.org/img/wn/${icon}.png"
+            src="http://openweathermap.org/img/wn/${city.icon}.png"
             alt=""
           />
-          <p class="card__day__info">${min}°/${max}°</p>
+          <p class="card__day__info">${city.min}°/${city.max}°</p>
         </span>
       `;
-  card.innerHTML = CardHTML;
+  card.innerHTML = cardHTML;
   cardContainer.appendChild(card);
   input.value = "";
 }
